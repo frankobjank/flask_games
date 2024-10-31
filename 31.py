90d3be62b8aa1615e89528c8f1304a69606a9366
Jacob Frank <frankobjank@gmail.com>
2024-10-31 00:32:54 -0400

    Adding cards Python from hotseat versions

---
+++ 31.py
@@ -0,0 +1,299 @@
+import shared_cards as shared
+import random
+import sys
+from time import sleep
+
+
+class CustomState(shared.State):
+    def __init__(self, ace_value=11, hand_size=3, max_players=7, random_names_flag=True) -> None:
+        super().__init__(ace_value, hand_size, max_players, random_names_flag)
+        
+        # modes : start, add_players, main_phase, discard, end_turn, end_round, end_game
+        # end_turn is specific to hotseat - otherwise, next turn can begin automatically
+        self.mode = "start"
+        
+        self.dead_players = {} # could get rid of this by making a Round class that only includes players that aren't dead yet
+        self.knocked = ""
+        self.discard = []
+        self.free_ride_alts = ["free ride", "on the bike", "on the dole", "riding the bus", "barely hanging on", "has a tummy ache", "has had a long day"]
+
+
+    def calc_hand_score(self, player_object):
+        hand_scores = {}
+        for card in player_object.hand:
+            if card.suit not in hand_scores.keys():
+                hand_scores[card.suit] = 0
+            hand_scores[card.suit] += card.value
+        return max(hand_scores.values())
+
+
+    def new_round_custom(self):
+        self.new_round()
+        self.discard = [self.draw_card()]
+        self.knocked = ""
+        for i in range(12):
+            print("\n")
+        print(f"--- ROUND {self.round.num} ---\n")
+        print("\n--- DEALING ---")
+
+
+    def new_game_custom(self):
+        self.new_game()
+        self.dead_players = {}
+
+
+    def start_round(self):
+        self.new_round_custom()
+        self.start_turn()
+
+
+    def end_round(self):
+        # scenarios - win doesn't actually matter, just display who knocked and loser
+            # BLITZ - everyone except highest loses a life
+            # tie for loser - display knocked; no one loses
+            # 1 loser - display one who knocked and one who lost
+            # 1 loser AND loser knocked - loser loses 2 points
+        self.sleep_print(f"--- END OF ROUND {self.round.num} ---\n")
+        self.sleep_print("---     SCORES     ---\n")
+        for player in self.player_order:
+            hand_score = f"{player} - {self.calc_hand_score(self.players[player])}"
+            if player == self.knocked:
+                hand_score += " - k"
+            self.sleep_print(f"{hand_score}")
+        
+        if self.check_for_blitz():
+            for player in self.player_order:
+                if player != self.round.current_player:
+                    print(f"{player} loses 1 extra life.")
+                    self.players[player].score -= 1
+        else:
+            hand_scores = [self.calc_hand_score(p_object) for p_object in self.players.values()]
+            lowest_hand_score = min(hand_scores)
+
+            if hand_scores.count(lowest_hand_score) > 1:
+                self.sleep_print("Tie for last place, no change in score.")
+            else:
+                lowest_hand_score_player = ""
+                for p_object in self.players.values():
+                    if self.calc_hand_score(p_object) == lowest_hand_score:
+                        lowest_hand_score_player = p_object.name
+                        
+                if lowest_hand_score_player != self.knocked:
+                    self.sleep_print(f"{lowest_hand_score_player} loses 1 extra life.")
+                    self.players[lowest_hand_score_player].score -= 1
+                else:
+                    self.sleep_print(f"{lowest_hand_score_player} knocked but had the lowest score.\n{lowest_hand_score_player} loses 2 extra lives.")
+                    self.players[lowest_hand_score_player].score -= 2
+        
+        knocked_out = [p_name for p_name, p_object in self.players.items() if 0 > p_object.score]
+        
+        for p_name in knocked_out:
+            self.sleep_print(f"{p_name} has been knocked out.")
+            self.dead_players[p_name] = self.players.pop(p_name)
+            # the only time player_order must be changed
+            self.player_order.remove(p_name)
+
+        if len(self.players) == 1:
+            winner = [p for p in self.players.keys()][0]
+            self.sleep_print(f"\n{winner} wins!")
+            self.mode = "end_game"
+        else:
+            self.mode = "end_round"
+            self.sleep_print("\nRemaining Players' Extra Lives:")
+            for p_name, p_object in self.players.items():
+                msg = f"{p_name} - {p_object.score} extra lives"
+                if p_object.score == 0:
+                    msg += f" - ({self.free_ride_alts[random.randint(0, len(self.free_ride_alts)-1)]})"
+                self.sleep_print(msg)
+            print("\n")
+
+
+    def start_turn(self):
+        # 2 msgs per turn, so num to display is 2*num_players-1
+        log_to_display =  self.round.round_log[-2*(len(self.player_order)-1):]
+        for msg in log_to_display:
+            print(msg)
+        self.round.turn_num += 1
+        print_players = ""
+        for p_name in self.player_order:
+            if p_name == self.round.current_player:
+                print_players += "*"
+            print_players += f"{p_name} "
+        print(f"\nPlayer order: {print_players}")
+        print(f"It is {self.round.current_player}'s turn.\n")
+
+        self.mode = "main_phase"
+
+
+    def end_turn(self):
+        self.round.current_player = self.player_order[((self.round.turn_num + ((self.round.num-1) % len(self.player_order))) % len(self.player_order))]
+        if self.round.current_player != self.knocked:
+            self.mode = "end_turn"
+        else:
+            self.end_round()
+
+
+    def check_for_blitz(self):
+        return self.calc_hand_score(self.players[self.round.current_player]) == 31
+
+
+    def get_user_input(self) -> dict:
+        user_input = ""
+        packet = {}
+        if self.mode == "start":
+            accepted_inputs = [str(i) for i in range(2, self.max_players+1)]
+            while user_input not in accepted_inputs:
+                user_input = input(f"How many players? Enter a number from 2-{self.max_players}.\n")
+            packet = self.user_input_to_packet(action="start", msg=user_input)
+        
+        elif self.mode == "add_player":
+            if self.random_names_flag:
+                # using while loop here removes the need to .pop and reset list every game
+                while user_input not in self.random_names_list:
+                    user_input = self.random_names_list[random.randint(0, len(self.random_names_list)-1)]
+            else:
+                while len(user_input) == 0:
+                    user_input = input("Please enter a name:\n")
+                    if len(user_input) > 12:
+                        user_input = user_input[:12]
+            packet = self.user_input_to_packet(action="add_player", msg=user_input)
+        
+        elif self.mode == "main_phase":
+            abbr_to_word = {"p": "pickup", "d": "draw", "k": "knock"}
+            while user_input not in abbr_to_word.values():
+                self.print_state()
+                user_input = input("Choose one: [p]ickup, [d]raw or [k]nock\n")
+                if user_input in abbr_to_word.keys():
+                    user_input = abbr_to_word[user_input]
+            packet = self.user_input_to_packet(action=user_input, msg="")
+        
+        elif self.mode == "discard":
+            accepted_inputs = [str(i+1) for i in range(len(self.players[self.round.current_player].hand))]
+            while user_input not in accepted_inputs:
+                self.print_state()
+                for i, card in enumerate(self.players[self.round.current_player].hand):
+                    print(f"[{i+1}]:{card}")
+                user_input = input(f"Enter the index of the card you want to discard:\n")
+            packet = self.user_input_to_packet(action="discard", msg=user_input)
+
+        elif self.mode == "end_turn" or self.mode == "end_round":
+            if self.mode == "end_turn":
+                for i in range(14):
+                    print("\n")
+            while len(user_input) == 0:
+                input("Enter any key to continue.\n")
+                user_input = "continue"
+            packet = self.user_input_to_packet(action=user_input, msg="")
+
+        elif self.mode == "end_game":
+            abbr_to_word = {"n": "new_game", "q": "quit"}
+            while user_input not in abbr_to_word.values():
+                user_input = input("\nChoose one: [n]ew game or [q]uit\n")
+                if user_input in abbr_to_word.keys():
+                    user_input = abbr_to_word[user_input]
+            packet = self.user_input_to_packet(action=user_input, msg="")
+
+        return packet
+
+
+    def user_input_to_packet(self, action, msg) -> dict:
+        # actions: start, add_player, draw, pickup, knock, discard, continue, new_game, quit
+        return {"action": action, "msg": msg}
+
+
+    def update(self, packet: dict):
+        # {"name": "", "action": "", "msg": ""}
+        # actions: start, add_player, draw, pickup, knock, discard, continue, new_game, quit
+        if self.mode == "start":
+            self.add_players(num_players=int(packet["msg"]))
+            for p_object in self.players.values():
+                # set extra lives (score) to +3
+                p_object.score += 3
+            self.mode = "add_player"
+
+        elif self.mode == "add_player" and packet["action"] == "add_player":
+            for p_name, p_object in self.players.items():
+                # player name starts as order: int. continue through str player names
+                if isinstance(p_name, str):
+                    continue
+                self.set_player_name(p_name, p_object, name_input=packet["msg"])
+                break
+
+            if all(isinstance(p_name, str) for p_name in self.players.keys()):
+                # set_player_order only needs to happen once per game
+                self.set_player_order()
+                self.start_round()
+
+        elif self.mode == "main_phase":
+            taken_card = None
+            if packet["action"] == "knock":
+                if len(self.knocked) > 0:
+                    print(f"{self.knocked} has already knocked. You must pick a different move.")
+                    return
+                self.knocked = self.round.current_player
+                print(f"{self.round.current_player} has knocked.")
+                self.round.round_log.append(f"{self.round.current_player} knocked.")
+                self.end_turn()
+                return
+
+            elif packet["action"] == "pickup":
+                # convert to str here for type consistency
+                taken_card = self.discard.pop()
+                print(f"\nYou pick up a {taken_card} from discard.")
+                self.round.round_log.append(f"{self.round.current_player} picked up a {taken_card} from discard.")
+
+            elif packet["action"] == "draw":
+                taken_card = self.draw_card()
+                print(f"\nYou draw: {taken_card}.")
+                self.round.round_log.append(f"{self.round.current_player} drew a card from the deck.")
+            
+            self.players[self.round.current_player].hand.append(taken_card)
+            self.mode = "discard"
+
+            if self.check_for_blitz():
+                print(f"{self.round.current_player} BLITZED!!!")
+                self.end_round()
+
+        elif self.mode == "discard" and packet["action"] == "discard":
+            discarded_card = self.players[self.round.current_player].hand.pop(int(packet["msg"])-1)
+            self.discard.append(discarded_card)
+            self.round.round_log.append(f"{self.round.current_player} discarded: {discarded_card}")
+            self.end_turn()
+        
+        elif self.mode == "end_turn":
+            if packet["action"] == "continue":
+                self.start_turn()
+        elif self.mode == "end_round":
+            if packet["action"] == "continue":
+                self.start_round()
+        elif self.mode == "end_game":
+            if packet["action"] == "new_game":
+                self.new_game_custom()
+                self.mode = "start"
+            elif packet["action"] == "quit":
+                raise SystemExit(0)
+
+
+    def sleep_print(self, *msg):
+        for m in msg:
+            sleep(.8)
+            print(m)
+    
+
+    def print_state(self):
+        if len(self.discard) > 0:
+            discard_card = self.discard[-1]
+        else:
+            discard_card = None
+        self.sleep_print(f"Current Hand: {self.players[self.round.current_player].hand}. Score: {self.calc_hand_score(self.players[self.round.current_player])}", f"Discard: {discard_card}")
+        
+
+# main loop
+def hotseat(random_names_flag=True):
+    state = CustomState(random_names_flag)
+    
+    while True:
+        packet = state.get_user_input()
+        state.update(packet)
+
+hotseat(random_names_flag=True)
---
+++ shared_cards.py
@@ -0,0 +1,145 @@
+import random
+from operator import attrgetter
+from enum import Enum
+
+# Try using decorators for inheriting functions
+
+# games: 31, cribbage, conquian, Battleship
+
+class Player:
+    def __init__(self, name="") -> None:
+        self.name = name
+        self.order = 0
+        self.hand = []
+        self.score = 0
+    
+    def __repr__(self) -> str:
+        return self.name
+
+    def sort_hand(self, suit=True) -> None:
+        # self.hand = sorted(sorted(self.hand, key=attrgetter("value"), reverse=True), key=attrgetter("suit"), reverse=True)
+        self.hand = sorted(sorted(self.hand, key=attrgetter("suit"), reverse=True), key=attrgetter("value"), reverse=True)
+
+class Card:
+    def __init__(self, rank: str, value: int, suit: str, suit_display: str):
+        self.rank = rank
+        self.value = value
+        self.suit = suit
+        self.suit_display = suit_display
+    
+    def __repr__(self) -> str:
+        return f"{self.rank}{self.suit_display}"
+
+class Deck:
+    def __init__(self, ace_value) -> None:
+        self.unshuffled_cards = []
+        self.shuffled_cards = []
+        self.ace_value = ace_value
+
+        ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+        cardtype_to_value = {"2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "10": 10, "J": 10, "Q": 10, "K": 10, "A": self.ace_value}
+        suits = ["spade", "heart", "diamond", "club"]
+        suit_to_display = {"spade": "\u2664", "heart": "\u2665", "diamond": "\u2666", "club": "\u2667"}
+
+        self.unshuffled_cards = [Card(rank, cardtype_to_value[rank], suit, suit_to_display[suit]) for suit in suits for rank in ranks]
+        # print("\u2660") ♠
+        # print("\u2661") ♡
+        # print("\u2662") ♢
+        # print("\u2663") ♣
+        # print("\u2664") ♤
+        # print("\u2665") ♥
+        # print("\u2666") ♦
+        # print("\u2667") ♧
+
+    def __repr__(self) -> str:
+        return f"Deck({self.shuffled_cards})"
+
+# counted as one "hand" (i.e. one cycle of turns before needing to shuffling again)
+class Round:
+    def __init__(self, player_order, num=0) -> None:
+        self.num = num
+        # current player and turn_num could be considered turn attributes but doesn't merit another class
+        self.turn_num = 0
+        self.round_log = []
+        
+        if len(player_order) > 0:
+            # increment first player and set dealer
+            first_player_index = ((num)-1) % len(player_order)
+            self.first_player = player_order[first_player_index]
+            self.current_player = player_order[first_player_index]
+            self.dealer = player_order[first_player_index-1]
+
+
+class State:
+    def __init__(self, ace_value: int, hand_size: int, max_players: int, random_names_flag: bool) -> None:
+        self.deck = Deck(ace_value)
+        self.hand_size = hand_size
+        self.max_players = max_players
+        self.players = {}
+        self.player_order = [] # static
+
+        # self.turn_num = 0 # global - putting into Round
+        self.log = [] # global - putting into Round
+        # self.log_to_display = []
+        # round.num defaults to 0 and increments on subsequent rounds
+        self.round = Round(self.player_order)
+
+        self.random_names_flag = random_names_flag
+        self.random_names_list = ["Henk", "Jenkins", "Stone", "Bubbles", "Pickles", "Skwisgaar", "Gertrude"]
+
+    def shuffle_deck(self) -> list:
+        shuffled_cards = []
+        cards_to_add = self.deck.unshuffled_cards.copy()
+
+        while len(cards_to_add) > 0:
+            randindex = random.randint(0, len(cards_to_add)-1)
+            shuffled_cards.append(cards_to_add.pop(randindex))
+        
+        assert len(self.deck.unshuffled_cards) == 52, "this should always be 52"
+
+        return shuffled_cards
+
+    def draw_card(self) -> Card:
+        return self.deck.shuffled_cards.pop()
+
+    def add_card_to_hand(self, player_object: Player) -> None:
+        player_object.hand.append(self.draw_card())
+        player_object.sort_hand()
+
+    def deal(self, player_object: Player) -> None:
+        for i in range(self.hand_size):
+            self.add_card_to_hand(player_object)
+
+    def set_player_order(self, override=[]) -> None:
+        if len(override) == 0:
+            self.player_order = [p_name for p_name in self.players.keys()]
+        else:
+            self.player_order = override
+
+    def add_players(self, num_players: int) -> None:
+        # name is stored as integer (order added)
+        for i in range(num_players):
+            self.players[i] = Player()
+
+    def set_player_name(self, p_name, p_object, name_input):
+        p_object.order = p_name
+        # change name from integer to chosen name
+        p_object.name = name_input
+        self.players[name_input] = self.players.pop(p_name)
+        print(f"Welcome {name_input}")
+
+
+    def new_game(self) -> None:
+        self.players = {}
+        self.player_order = []
+        self.round = Round(self.player_order)
+
+    def new_round(self, custom_round=False) -> None:
+        if not custom_round:
+            self.round = Round(self.player_order, self.round.num+1)
+            
+        self.deck.shuffled_cards = self.shuffle_deck()
+        for p_object in self.players.values():
+            p_object.hand = []
+            self.deal(p_object)
+
