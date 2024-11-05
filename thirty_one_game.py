3d31441b769d198bc343bb0fed8bc461c7a5396d
Jacob Frank <frankobjank@gmail.com>
2024-11-05 00:51:54 -0500

    Reformatting 31 python file

--- thirty_one_game.py
+++ thirty_one_game.py
@@ -1,22 +1,136 @@
-import shared_cards as shared
+from operator import attrgetter
 import random
 
-class CustomState(shared.State):
+class Player:
+    def __init__(self, name="") -> None:
+        self.name = name
+        self.order = 0
+        self.hand = []
+        self.score = 0
+    
+    
+    def __repr__(self) -> str:
+        return f"{Player(self.name)}"
+
+    
+    def sort_hand(self) -> None:
+        """Sort hand in order of suit."""
+
+        self.hand = sorted(sorted(self.hand, key=attrgetter("suit"), reverse=True), key=attrgetter("value"), reverse=True)
+    
+
+    def zip_hand(self):
+        """Convert hand to portable strings to send to client."""
+        
+        cards = []
+        for c in self.hand:
+            cards.append(c.zip_self())
+        
+        return cards
+
+
+class Card:
+    def __init__(self, rank: str, value: int, suit: str, suit_display: str):
+        self.rank = rank
+        self.value = value
+        self.suit = suit
+        self.suit_display = suit_display
+    
+    
+    def __str__(self) -> str:
+        return f"{self.rank}{self.suit_display}"
+    
+
+    def zip_self(self):
+        """Create portable string to send to client."""
+        
+        # Convert 10 to T to make all cards 2 chars long
+        rank = self.rank
+        if rank == "10":
+            rank = "T"
+
+        # returns 2S, 3C, AH, etc.
+        return f"{rank}{self.suit[0].capitalize()}"
+    
+
+class Deck:
+    def __init__(self, ace_value) -> None:
+        self.unshuffled_cards = []
+        self.shuffled_cards = []
+        self.ace_value = ace_value
+
+        ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+        cardtype_to_value = {"2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "10": 10, "J": 10, "Q": 10, "K": 10, "A": self.ace_value}
+        suits = ["spade", "heart", "diamond", "club"]
+        suit_to_display = {"spade": "\u2664", "heart": "\u2665", "diamond": "\u2666", "club": "\u2667"}
+
+        self.unshuffled_cards = [Card(rank, cardtype_to_value[rank], suit, suit_to_display[suit]) for suit in suits for rank in ranks]
+
+
+    def __str__(self) -> str:
+        return f"Deck({self.shuffled_cards})"
+
+
+class State:
     def __init__(self, ace_value=11, hand_size=3, max_players=7) -> None:
-        super().__init__(ace_value, hand_size, max_players)
         
         # modes : start, add_players, main_phase, discard 
-        #   HANDLE THESE MODES internally instead: end_turn, end_round, end_game
+            # HANDLE THESE MODES internally instead: end_turn, end_round, end_game
         
+        # Game pieces
+        self.deck = Deck(ace_value)
+        self.hand_size = hand_size
+        self.max_players = max_players
+        self.players = {}
+        self.player_order = []  # static
+        self.dead_players = {}  # I think this can be removed without any consequences
+
+        # Gameplay
         self.mode = "start"
-        
-        self.dead_players = {} # could get rid of this by making a Round class that only includes players that aren't dead yet
-        self.knocked = ""
+        self.in_progress = False
+        self.log = []
+
+        # Rounds
+        self.round_num = 0
+        self.turn_num = 0
+        self.first_player = ""  # player name
+        self.current_player = ""  # player name
+        self.dealer = ""  # player name
+        self.knocked = ""  # player name
         self.discard = []
         self.free_ride_alts = ["getting a free ride", "on the bike", "on the dole", "riding the bus", "barely hanging on", "having a tummy ache", "having a long day"]
 
+        # Websocket id
+        self.sid = ""
 
-    def calc_hand_score(self, player_object:shared.Player) -> int:
+        
+
+    def shuffle_deck(self) -> list:
+        shuffled_cards = []
+        cards_to_add = self.deck.unshuffled_cards.copy()
+
+        while len(cards_to_add) > 0:
+            randindex = random.randint(0, len(cards_to_add)-1)
+            shuffled_cards.append(cards_to_add.pop(randindex))
+        
+        assert len(self.deck.unshuffled_cards) == 52, "This list should remain unchanged."
+
+        return shuffled_cards
+
+
+    def draw_card(self) -> Card:
+        return self.deck.shuffled_cards.pop()
+
+
+    def deal(self, player_object: Player) -> None:
+        """Deal starting hands."""
+
+        for i in range(self.hand_size):
+            player_object.hand.append(self.draw_card())
+        player_object.sort_hand()
+
+
+    def calc_hand_score(self, player_object:Player) -> int:
         hand_scores = {}
         for card in player_object.hand:
             if card.suit not in hand_scores.keys():
@@ -25,22 +139,63 @@ class CustomState(shared.State):
         return max(hand_scores.values())
 
 
-    def new_game_31(self):
-        self.new_game()
+    def reset_game(self) -> None:
+        # Players will not need to be reset every game
+        self.player_order = []
+        self.round_num = 0
         self.dead_players = {}
 
 
-    def start_round(self):
-        # Resets variables in shared
+    def start_game(self) -> None:
+        # Initial setup
+        assert self.round_num == 0, "Round number must be 0."
+
+        # Check number of players
+        if not (2 <= len(self.players) <= 7):
+            return "Need between 2 and 7 players to begin."
+        
+        # Set player order - eventually should be random
+        self.player_order = [p_name for p_name in self.players.keys()]
+
+        # Set in progress to True
+        self.in_progress = True
+
         self.new_round()
 
-        # Resets 31-specific variables
+
+    def new_round(self) -> None:
+
+        assert len(self.player_order) != 0, "Player order must be set before round begins"
+        
+        self.round_num += 1
+        self.turn_num = 0
+            
+        # Set dealer, first player of round
+        first_player_index = ((self.round_num)-1) % len(self.player_order)
+        self.first_player = self.player_order[first_player_index]
+        self.current_player = self.player_order[first_player_index]
+        self.dealer = self.player_order[first_player_index-1]
+
+        # Shuffle cards
+        self.deck.shuffled_cards = self.shuffle_deck()
+        
+        # Reset each player's hand and deal new hand
+        for p_object in self.players.values():
+            p_object.hand = []
+            self.deal(p_object)
+
+        # Set a discard card; reset knocked
         self.discard = [self.draw_card()]
         self.knocked = ""
         
-        print(f"\n--- ROUND {self.round.num} ---\n")
+        print(f"\n--- ROUND {self.round_num} ---\n")
+        self.log.append(f"\n--- ROUND {self.round_num} ---\n")
         print("\n--- DEALING ---")
+        self.log.append("\n--- DEALING ---")
+
+        # Move on to turn
         self.start_turn()
+
 
 
     def end_round(self):
@@ -50,7 +205,7 @@ class CustomState(shared.State):
             # 1 loser - display one who knocked and one who lost
             # 1 loser AND loser knocked - loser loses 2 points
         
-        print(f"--- END OF ROUND {self.round.num} ---\n")
+        print(f"--- END OF ROUND {self.round_num} ---\n")
         print("---     SCORES     ---\n")
         
         if self.check_for_blitz():
@@ -107,7 +262,7 @@ class CustomState(shared.State):
 
     def start_turn(self):
         # Increment turn number
-        self.round.turn_num += 1
+        self.turn_num += 1
 
         # Set mode to main phase
         self.mode = "main_phase"
@@ -118,10 +273,10 @@ class CustomState(shared.State):
 
     def end_turn(self):
         # Calculate new current player
-        self.round.current_player = self.player_order[((self.round.turn_num + ((self.round.num-1) % len(self.player_order))) % len(self.player_order))]
+        self.current_player = self.player_order[((self.turn_num + ((self.round_num-1) % len(self.player_order))) % len(self.player_order))]
         
         # If new current player has knocked, round should end
-        if self.round.current_player == self.knocked:
+        if self.current_player == self.knocked:
             self.end_round()
 
 
@@ -142,14 +297,7 @@ class CustomState(shared.State):
             self.mode = "add_player"
 
         elif self.mode == "add_player" and packet["action"] == "add_player":
-            for p_name, p_object in self.players.items():
-                
-                # player name starts as order: int. continue through str player names
-                if isinstance(p_name, str):
-                    continue
-                
-                self.set_player_name(p_name, p_object, name_input=packet["msg"])
-                break
+
 
             if all(isinstance(p_name, str) for p_name in self.players.keys()):
                 # set_player_order only needs to happen once per game
